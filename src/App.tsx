ce86c50a6a954a23b064f613efd95ff27eae5742
Riey <creeper844@gmail.com>
2022-10-17 15:52:38 +0900

    Remove Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import Console from "./emuera/Console";
-import { HashRouter, Routes, Route } from "react-router-dom";
 
 const FullscreenApp = styled.div`
   display: flex;
@@ -13,11 +12,7 @@ function App() {
 
   return (
     <FullscreenApp>
-      <HashRouter>
-        <Routes>
-          <Route index path="/" element={<Console />} />
-        </Routes>
-      </HashRouter>
+      <Console />
     </FullscreenApp>
   );
 }
